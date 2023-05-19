53616fec3f398d1d91fe83519c0e0c7e15beffab
Nisal Bandara <nisal.bandara@outlook.com>
2023-05-19 09:53:53 +0530

    misc: use light background and vscode split in init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.cmd([[
+  set background=light
+]])
+
+if vim.g.vscode then
+  require("vscode")
+else
+  require("config.lazy")
+end
